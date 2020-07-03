34b39c758408dba582519d63a077daf6d56569c0
Drenii <dreni107@hotmail.com>
2020-07-03 02:32:28 +0200

    Projekti ne Android Studio

---
+++ app/src/main/java/com/example/mathquiz2/HomeActivity.java
@@ -0,0 +1,40 @@
+package com.example.mathquiz2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class HomeActivity extends AppCompatActivity {
+    Button logout;
+    Button btnPlay;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        logout=findViewById(R.id.logout);
+        btnPlay=findViewById(R.id.btnPlay);
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(HomeActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intToMain=new Intent(HomeActivity.this,MainActivity2.class);
+                startActivity(intToMain);
+            }
+        });
+    }
+}
